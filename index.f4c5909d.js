977a6e8f183dcb6dca7d47d52153f3cdef16409c
Viktor <victor.tsupa@gmail.com>
2024-02-27 16:25:31 +0200

    make build

---
+++ index.f4c5909d.js
@@ -0,0 +1,3 @@
+document.addEventListener("click",function(e){e.target.matches(".icon--burger")?document.body.style.overflow="hidden":e.target.matches(".icon--close,.menu__link")&&(document.body.style.overflow="auto")}),document.querySelector(".subscribe__button").addEventListener("click",function(e){e.preventDefault(),document.querySelector(".subscribe__input").value=""});//# sourceMappingURL=index.f4c5909d.js.map
+
+//# sourceMappingURL=index.f4c5909d.js.map
